e4bbd4f711a1881ab130835b8500fcfe066d52b4
shechin <shechinmihail@mail.ru>
2022-08-30 22:05:09 +0500

    Начал ДЗ 1.10

--- src/Lesson9/Author.java
+++ src/Lesson9/Author.java
@@ -24,4 +24,5 @@ public class Author {
                 ", lostName='" + lostName + '\'' +
                 '}';
     }
+
 }
--- src/Lesson9/Book.java
+++ src/Lesson9/Book.java
@@ -36,4 +36,6 @@ public class Book {
                 ", yearOfPublication=" + yearOfPublication +
                 '}';
     }
+
+
 }
