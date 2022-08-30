28c079a30d7028e8a654ff894c591d3d721fa3e9
shechin <shechinmihail@mail.ru>
2022-08-30 22:40:59 +0500

    Выполнил ДЗ 1.10, написал задание

--- src/Lesson9/Main.java
+++ src/Lesson9/Main.java
@@ -10,13 +10,21 @@ public class Main {
 
         book2.setYearOfPublication(2010);
 
+        System.out.println(book1.getName() + " " + book1.getAuthor() + " " + book1.getYearOfPublication());
+
+        // Task 10
+        // Реализовать методы toString, equals и hashCode в классах Author и Book,
+        // которые были созданы на прошлом уроке. Обратите внимание,
+        // что toString книги не должен дублировать код из toString автора,
+        // а должен делегировать (вызывать) его версию метода.
+
         System.out.println(book2);
 
         System.out.println(author.equals(author1));
 
-        System.out.println(book2.hashCode());
+        System.out.println(book2.hashCode() == book1.hashCode());
 
-        System.out.println(book1.getName() + " " + book1.getAuthor() + " " + book1.getYearOfPublication());
+
 
     }
 
